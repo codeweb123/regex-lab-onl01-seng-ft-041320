a13b85e215b7b8caa1422234ef1ef825255aed72
Jiyon FL <jiyonj@hotmail.com>
2020-04-27 21:46:24 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,9 +11,12 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/\A[A-Z]\w*\W\b/) !=nil
 end
 
-def valid_phone_number?(phone)
-
+if phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
+    true
+  else
+    false
+  end
 end
