057119a084952501421c245a81a13028064ac218
Jiyon FL <jiyonj@hotmail.com>
2020-04-27 21:57:30 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation_2?(text)
-  if text.match(/\w[A-Z]/) && text.match(/[\W]/)
+  if text.match(/\w[A-Z]/) && text.match(/\W/)
     true
   else
     false
