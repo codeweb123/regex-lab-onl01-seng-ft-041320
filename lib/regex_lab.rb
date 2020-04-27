f50f0601b77503b4b9fa99381823687e7d81d3c5
Jiyon FL <jiyonj@hotmail.com>
2020-04-27 21:48:04 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -10,8 +10,12 @@ def words_five_letters_long(text)
   text.scan(/\b\w{5}\b/)
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\A[A-Z]\w*\W\b/) !=nil
+def first_word_capitalized_and_ends_with_punctuation_2?(text)
+  if text.match(/^[A-Z]/) && text.match(/[\.]$/)
+    true
+  else
+    false
+  end
 end
 
 if phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
