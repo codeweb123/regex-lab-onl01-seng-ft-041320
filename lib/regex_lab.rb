54e9405c0482788209e7685a2a8905cc01b87044
Jiyon FL <jiyonj@hotmail.com>
2020-04-27 21:52:03 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation_2?(text)
-  if text.match(/^[A-Z]/) && text.match(/[\.]$/)
+  if text.match(/^[A-Z]/) && text.match(/[\W]$/)
     true
   else
     false
